134d15cead8ff6950287dbc847e5a61940d6122d
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2018-12-08 00:01:57 +0100

    update website

--- js/sketch.js
+++ js/sketch.js
@@ -22,8 +22,18 @@ if (selectedText != '') {
         clearInterval(SI);
       }, 2000);
       myDiv.innerHTML = 'BASEL.CODES<br>📆<br>09.02.19<br><a href="http://www.hek.ch/" target="_blank" rel="noopener noreferrer">@H3K🏛</a> && <a href="https://www.fhnw.ch/de/die-fhnw/hochschulen/hgk" target="_blank" rel="noopener noreferrer">@HGK🏫</a><br>📣<br><a href="https://docs.google.com/forms/d/e/1FAIpQLScsw6eTpP0SagAixIytgZOCWeMpwjwcjG1QJT7PN9V0kR8xIA/viewform" target="_blank" rel="noopener noreferrer">OPEN CALL</a><br>FOR WORKSHOPS AND PRESENTATIONS'
-    //   $('#title-animation').lettering();
+      //   $('#title-animation').lettering();
     }
   }, 100);
 
+}
+
+window.onmousemove = (event) => {
+  // console.log(event.clientX, event.clientY);
+  const x = ((event.clientX / innerWidth) - 0.5) * 2;
+  const y = ((event.clientY / innerHeight) - 0.5) * 2;
+  // console.log(x, y);
+  const shadow = '#00f ' + x * 5 + 'px ' + y * 5 + 'px';
+  // console.log(shadow);
+  document.getElementById('title-animation').style.textShadow = shadow;
 }
