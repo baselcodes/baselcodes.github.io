b0e48f1f48637cdb7628908af2e5b1b541627763
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2019-09-15 18:01:07 +0200

    update website with call to squatted

--- js/sketch.js
+++ js/sketch.js
@@ -1,66 +1,3 @@
-<<<<<<< HEAD
-//const NEW_LINE = '$'
-const TITLE = 'basel.codes 📆 09.02.19 🏛 @h3k && 🏫 @hgk 📣 open call for workshops and presentations 🤟'
-
-
-let index = 0;
-let selectedText = '';
-let write = true;
-selectedText = TITLE.split('');
-if (selectedText != '') {
-  SI = setInterval(() => {
-    let myDiv = document.getElementById('title-animation');
-    if (write) {
-      let letter = selectedText[index];
-      //if (letter === NEW_LINE) letter = '<br>'
-      myDiv.innerHTML += letter;
-      myDiv.scrollTop = myDiv.scrollHeight;
-      index++;
-    }
-    if (index >= selectedText.length) {
-      write = false;
-      setTimeout(() => {
-        clearInterval(SI);
-      }, 200);
-      myDiv.innerHTML = '<p>basel.codes</p><p>📆 09.02.19</p><a href="http://www.hek.ch/" target="_blank" rel="noopener noreferrer">🏛 @h3k</a><p>&&</p><a href="https://www.fhnw.ch/de/die-fhnw/hochschulen/hgk" target="_blank" rel="noopener noreferrer">🏫 @hgk</a><a href="https://docs.google.com/forms/d/e/1FAIpQLScsw6eTpP0SagAixIytgZOCWeMpwjwcjG1QJT7PN9V0kR8xIA/viewform" target="_blank" rel="noopener noreferrer">📣 open call for workshops and presentations</a><a href="mailto:helloworld@basel.codes">🤟</a>'
-      //   $('#title-animation').lettering();
-    }
-  }, 10);
-
-}
-
-window.onmousemove = (event) => {
-  // console.log(event.clientX, event.clientY);
-  const x = ((event.clientX / innerWidth) - 0.5) * 2;
-  const y = ((event.clientY / innerHeight) - 0.5) * 2;
-  // console.log(x, y);
-  const shadow = '#ff0 ' + x * 5 + 'px ' + y * 5 + 'px';
-  // console.log(shadow);
-  // document.getElementById('title-animation').style.textShadow = shadow;
-  
-}
-
-let cnv;
-let balls = [];
-function setup() {
-  cnv = createCanvas(innerWidth, innerHeight);
-  cnv.parent('p5')
-  for (let i = 0; i < 5; i++)balls.push(new Ball());
-}
-
-function draw() {
-  background(200);
-  for (const ball of balls) {
-    ball.update();
-    // ball.proximity(balls);
-    ball.show(balls);
-  }
-}
-
-function windowResized() {
-  resizeCanvas(innerWidth, innerHeight);
-}
-=======
 let cnv;
 let bball;
 function setup() {
@@ -109,4 +46,3 @@ class Bounce_Ball {
             ellipse(this.pos.x, this.pos.y, this.r);
     }
 }
->>>>>>> b6a675aaff6934fa6c8bf9980d468a8329cbc73d
