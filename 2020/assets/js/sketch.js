68bc50c085bdefc062e0ad57546c15051b11a400
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2020-08-26 16:24:14 +0200

    add sober animation

--- 2020/assets/js/sketch.js
+++ 2020/assets/js/sketch.js
@@ -2,6 +2,9 @@
 let PCD
 let PCD_shader
 let matcap
+let wave = 0
+
+const inc = Math.PI / 100
 
 function preload() {
   PCD = loadModel('assets/3d/Samsung Laptop obj/laptop_open.obj')
@@ -17,23 +20,31 @@ function setup() {
 }
 
 function draw() {
-  scale(50)
+  scale(10)
   // ortho(-1500, 1500, -1500, 1500, -1500, 1500)
   // ortho()
-  background(random(255))
+  // background(random(255))
+  background(255)
   shader(PCD_shader)
 
   // Send the texture to the shader
   PCD_shader.setUniform("uMatcapTexture", matcap)
-  for (let i = -1; i <= 1; i++) {
-    push()
-    translate(i * 10, 2, 0)
-    // normalMaterial()
-    rotateX(PI - PI / 6)
-    rotateY(PI + (frameCount * 0.1 * i))
-    if(i == 0)rotateX(frameCount * 0.1)
-    // rotateZ(frameCount * 0.1)
-    model(PCD)
-    pop()
+  for (let j = -3; j <= 3; j++) {
+    for (let i = -6; i <= 6; i++) {
+      push()
+      // translate(i * (sin(wave) * 2), 2, 0)
+      translate(i * 10, 2 + (j * 10), 0)
+      // translate(i * (sin(wave) * 2), 2, i * (sin(wave) * 2))
+      // normalMaterial()
+      rotateX(PI - PI / 6)
+      rotateY(PI)
+      rotateY(PI + (frameCount * 0.01 * i))
+      if (i == 0) rotateY(sin(wave) * PI)
+      // rotateZ(frameCount * 0.1)
+      // if(i !== 0)model(PCD)
+      model(PCD)
+      pop()
+    }
   }
+  wave += inc
 }
