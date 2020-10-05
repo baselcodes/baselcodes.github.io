bc09b5223385b0fb9d068e6c3528fdee2ced3ca6
ffd8 <ted@ffd8.org>
2020-10-06 00:06:29 +0200

    grid systems!
    - mapped grid for laptops
    - more rotation control w/ mouse
    - sin scale
    - need link to form...

--- 2020/assets/js/sketch.js
+++ 2020/assets/js/sketch.js
@@ -1,4 +1,3 @@
-//draw a spinning octahedron
 let PCD
 let PCD_shader
 let matcap
@@ -8,43 +7,47 @@ const inc = Math.PI / 100
 
 function preload() {
   PCD = loadModel('assets/3d/Samsung Laptop obj/laptop_open.obj')
-
   PCD_shader = loadShader("assets/shader/shader.vert", "assets/shader/shader.frag")
-
   matcap = loadImage("assets/shader/matcap.png")
 }
 
 function setup() {
-  createCanvas(innerWidth, innerHeight, WEBGL)
+  createCanvas(windowWidth, windowHeight, WEBGL)
   noStroke()
 }
 
 function draw() {
-  scale(10)
-  // ortho(-1500, 1500, -1500, 1500, -1500, 1500)
-  // ortho()
-  // background(random(255))
-  background(255)
+  background(127.5)
   shader(PCD_shader)
 
   // Send the texture to the shader
   PCD_shader.setUniform("uMatcapTexture", matcap)
-  for (let j = -3; j <= 3; j++) {
-    for (let i = -6; i <= 6; i++) {
+
+  let gridSize = 8;
+  for (let j = 0; j <= gridSize; j++) {
+    for (let i = 0; i <= gridSize; i++) {
       push()
+      let x = map(i, 0, gridSize-1, -width/2, width/2)
+      let y = map(j, 0, gridSize-1, -height/2, height/2)
       // translate(i * (sin(wave) * 2), 2, 0)
-      translate(i * 10, 2 + (j * 10), 0)
+      translate(x, y, 0)
+      scale(map(sin(frameCount*.001), -1, 1, 2, width/30))
       // translate(i * (sin(wave) * 2), 2, i * (sin(wave) * 2))
       // normalMaterial()
       rotateX(PI - PI / 6)
-      rotateY(PI)
-      rotateY(PI + (frameCount * 0.01 * i))
-      if (i == 0) rotateY(sin(wave) * PI)
-      // rotateZ(frameCount * 0.1)
-      // if(i !== 0)model(PCD)
+      let a = atan2(mouseY - height / 2, mouseX - width / 2);
+      rotateX(a);
+      rotateY(a);
+      rotateY(PI + (i * .15 + frameCount * .01))
+      // if (i == 0) rotateY(sin(wave) * PI)
+      // rotateZ(frameCount * 0.01)
       model(PCD)
       pop()
     }
   }
   wave += inc
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
